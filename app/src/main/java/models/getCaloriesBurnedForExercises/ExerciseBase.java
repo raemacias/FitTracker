c0447e0820878f90b4a4c6a705a2cc459c5bfbf5
Rae <raemmacias@gmail.com>
2018-11-05 14:28:42 -0700

    Fixed broken file.

--- app/src/main/java/models/getCaloriesBurnedForExercises/ExerciseBase.java
+++ app/src/main/java/models/getCaloriesBurnedForExercises/ExerciseBase.java
@@ -4,22 +4,22 @@ import java.util.List;
 
 public class ExerciseBase {
 
-        private List<Exercise> exercises;
+    private List<Exercise> exercises;
 
-        public ExerciseBase() {
+    public ExerciseBase() {
 
-        }
-        public ExerciseBase(List<Exercise> exercises) {
-
-            this.exercises = exercises;
-        }
-        public List<Exercise> getExercises() {
-
-            return exercises;
-        }
-        public void setExercises(List<Exercise> exercises) {
-
-            this.exercises = exercises;
-        }
     }
+    public ExerciseBase(List<Exercise> exercises) {
+
+        this.exercises = exercises;
+    }
+    public List<Exercise> getExercises() {
+
+        return exercises;
+    }
+    public void setExercises(List<Exercise> exercises) {
+
+        this.exercises = exercises;
+    }
+}
 
